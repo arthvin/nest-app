c6f89f567b3a02451e7f2522df71b19ab91cbde8
arthurSemensati <arth.vin1408@gmail.com>
2024-11-21 15:00:14 -0300

    Commit

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -4,17 +4,20 @@ import { PassportModule } from '@nestjs/passport';
 import { UsersModule } from '../users/users.module';
 import { AuthService } from './auth.service';
 import { JwtStrategy } from './jwt.strategy';
+import { AuthController } from './auth.controller';
+
 
 @Module({
-  imports: [
-    UsersModule,
-    PassportModule,
-    JwtModule.register({
-      secret: 'secretKey', 
-      signOptions: { expiresIn: '1h' },
-    }),
-  ],
-  providers: [AuthService, JwtStrategy],
-  exports: [AuthService],
-})
-export class AuthModule {}
+    imports: [
+      UsersModule,
+      PassportModule,
+      JwtModule.register({
+        secret: 'secretKey', 
+        signOptions: { expiresIn: '1h' },
+      }),
+    ],
+    controllers: [AuthController], 
+    providers: [AuthService],
+    exports: [AuthService],
+  })
+  export class AuthModule {}
